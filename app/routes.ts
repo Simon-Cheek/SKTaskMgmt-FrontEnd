2514a8eaa8bf6c899fd015eb81e27c7c1f0cb644
Simon-Cheek <simon.cheek@yahoo.com>
2025-08-14 09:54:54 -0400

    Begin auth context and login page

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("pages/home.tsx"),
-  route("about", "./pages/about.tsx"),
+  index("pages/Home.tsx"),
+  route("about", "./pages/About.tsx"),
+  route("login", "./pages/Login.tsx"),
 ] satisfies RouteConfig;
