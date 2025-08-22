6cc6ae0e097deeb723763f4a5604d841fd8fb625
Simon-Cheek <simon.cheek@yahoo.com>
2025-08-22 08:03:57 -0400

    Start over

--- app/routes.ts
+++
@@ -1,7 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("pages/Home.tsx"),
-  route("about", "./pages/About.tsx"),
-  route("login", "./pages/Login.tsx"),
-] satisfies RouteConfig;
